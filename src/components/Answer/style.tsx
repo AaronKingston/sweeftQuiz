87c5bbdae506ddc7e72e33979eefdd8d0f690e75
AaronKingston <beka.sokurashvili@gmail.com>
2024-04-02 18:06:03 +0400

    Minor styling change in Answers component

--- src/components/Answer/style.tsx
+++ src/components/Answer/style.tsx
@@ -6,10 +6,8 @@ const getStyleObj = () => {
     answerContainer: {
       marginTop: 16,
       paddingVertical: 8,
-      //borderRadius: 2,
       borderWidth: 1,
       borderColor: colors.secondaryPink,
-      backgroundColor: colors.primaryViolet,
       paddingHorizontal: 16,
       flexDirection: 'row',
       justifyContent: 'space-between',
@@ -18,14 +16,13 @@ const getStyleObj = () => {
     answerText: {
       color: colors.light,
       fontSize: 16,
-      //fontWeight: 'bold',
       width: '80%',
     },
     check: {
       width: 26,
       height: 26,
       borderRadius: 2,
-      borderWidth: 2,
+      borderWidth: 1,
       borderColor: colors.neutralPink,
     },
   });
